c65f24043ea5451639355fd6a233f602fbc1e28e
Rahul9214 <147570570+Rahul9214@users.noreply.github.com>
2024-02-06 02:32:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,157 @@
+let video = document.querySelector("video");
+let recordBtnCont = document.querySelector(".record-btn-cont");
+let recordBtn = document.querySelector(".record-btn");
+let captureBtnCont = document.querySelector(".capture-btn-cont");
+let captureBtn = document.querySelector(".capture-btn");
+let recordFlag = false;
+let transparentColor = "transparent";
+
+
+let recorder;
+let chunks = [];   // Media data in chunks
+
+let constraints = {
+    video: true,
+    audio: true
+}
+
+
+// navigator -> global, browser info
+navigator.mediaDevices.getUserMedia(constraints)
+.then((stream) => {
+video.srcObject = stream;
+
+    recorder = new MediaRecorder(stream);
+    recorder.addEventListener("start", (e) => {
+        chunks = [];
+    })
+    recorder.addEventListener("dataavailable", (e) => {
+        chunks.push(e.data);
+})
+recorder.addEventListener("stop", (e) => {
+    // Conversion of media chunks data to video
+    let blob = new Blob(chunks, { type: "video/mp4" });
+   // let videoURL = URL.createObjectURL(blob);
+
+   if (db) {
+    let videoID = shortid();
+    let dbTransaction = db.transaction("video", "readwrite");
+    let videoStore = dbTransaction.objectStore("video");
+    let videoEntry = {
+        id:`vid-${videoID}`,
+        blobData: blob
+    }
+    videoStore.add(videoEntry);
+   }
+     
+    // For downloading video code below
+
+    // let a = document.createElement("a");
+   //  a.href = videoURL;
+   //  a.download = "stream.mp4";
+   //  a.click();
+  })
+})
+
+recordBtnCont.addEventListener("click", (e) => {
+    if (!recorder) return;
+
+    recordFlag = !recordFlag;
+
+    if (recordFlag) {  // Start
+       recorder.start();
+       recordBtn.classList.add("scale-record");
+       startTimer();
+    }
+    else {   // Stop
+       recorder.stop();
+       recordBtn.classList.remove("scale-record");
+       stopTimer();
+    }
+})
+
+captureBtnCont.addEventListener("click", (e) => {
+    captureBtn.classList.add("scale-capture");
+
+    let canvas = document.createElement("canvas");
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    let tool = canvas.getContext("2d");
+    tool.drawImage(video, 0, 0, canvas.width, canvas.height);
+    // Filtering process
+    tool.fillStyle = transparentColor;
+    tool.fillRect(0, 0, canvas.width, canvas.height);
+
+    let imageURL = canvas.toDataURL();
+
+    if (db) {
+        let imageID = shortid();
+        let dbTransaction = db.transaction("image", "readwrite");
+        let imageStore = dbTransaction.objectStore("image");
+        let imageEntry = {
+            id:`img-${imageID}`,
+            url: imageURL
+        }
+        imageStore.add(imageEntry);
+       }
+
+    // For Downloading image code below
+
+   // let a = document.createElement("a");
+   // a.href = imageURL;
+   // a.download = "image.jpg";
+   // a.click();
+
+   setTimeout(() => {
+    captureBtn.classList.remove("scale-capture");
+   }, 500)
+})
+
+let timerID;
+let counter = 0;  // Represents total seconds
+let timer = document.querySelector(".timer");
+function startTimer() {
+    timer.style.display = "block";
+    function displayTimer() {
+        let totalSeconds = counter;
+
+        let hours = Number.parseInt(totalSeconds / 3600);
+        totalSeconds = totalSeconds % 3600;   // Remaining value
+
+        let minutes = Number.parseInt(totalSeconds / 60);
+        totalSeconds = totalSeconds % 60;   // Remaining value
+
+        let seconds = totalSeconds;
+
+        hours = (hours < 10) ? `0${hours}` : hours;
+        minutes = (minutes < 10) ? `0${minutes}` : minutes;
+        seconds = (seconds < 10) ? `0${seconds}` : seconds;
+
+        timer.innerText = `${hours}:${minutes}:${seconds}`;
+        counter++;
+    }
+    timerID = setInterval(displayTimer, 1000);
+}
+
+function stopTimer() {
+    clearInterval(timerID);
+    timer.innerText = "00:00:00";
+    timer.style.display = "none";
+}
+
+
+// Filtering Logic
+let filterLayer = document.querySelector(".filter-layer");
+let allFilters = document.querySelectorAll(".filter");
+allFilters.forEach((filterElem) => {
+    filterElem.addEventListener("click", (e) => {
+        // Get style instead of set
+        transparentColor = getComputedStyle(filterElem).getPropertyValue("background-color");
+        filterLayer.style.backgroundColor = transparentColor;    // set style 
+    })
+})
+
+
+
+

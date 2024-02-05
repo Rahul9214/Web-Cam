8fc7b1af787a1c7e87fde9ac2e0cf737dc28fd67
Rahul Ranjan <rahsonu2u@gmail.com>
2024-02-06 02:47:08 +0530

    Delete Icon Bar directory

--- Icon Bar/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".container")
-.addEventListener("click", function() {
-    let iconBar = document.querySelector(".icon-bar");
-    if (iconBar.style.display === "none") {
-       iconBar.style.display = "block";
-    } else {
-        iconBar.style.display = "none";
-    }
-});
